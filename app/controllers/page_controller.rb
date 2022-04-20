b78857b95e430c7f5a90e301445bba8431ee6a90
Chim Kan <designium@gmail.com>
2022-04-20 12:32:48 -0400

    added placeholder pages for next creations.

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -13,4 +13,29 @@ class PageController < ApplicationController
 
   def loanrequest
   end
+
+  def shopping_product_listing
+  end
+
+  def shopping_category_page
+  end
+
+  def hotel_listing
+  end
+
+  def hotel_listing_details
+  end
+
+  def shopping_cart
+  end
+
+  def payment_step
+  end
+
+  def concert_listing
+  end
+
+  def concert_details
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,16 @@ Rails.application.routes.draw do
   get 'page/login'
   get 'page/login2'
   get 'page/loanrequest'
+  get 'page/shopping_product_listing'
+  get 'page/shopping_category_page'
+  get 'page/hotel_listing'
+  get 'page/hotel_listing_details'
+  get 'page/shopping_cart'
+  get 'page/payment_step'
+  get 'page/concert_listing'
+  get 'page/concert_details'
+
+
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
